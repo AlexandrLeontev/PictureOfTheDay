95ee3c08d2c396df2803083bd8b2ecde3bbbba0d
Alexandr <Almixleon@yandex.ru>
2021-07-01 16:07:19 +0400

    Начал изучение материального дизайна

---
+++ app/src/main/java/geekbarains/material/ui/picture/PictureOfTheDayViewModel.kt
@@ -0,0 +1,56 @@
+package geekbarains.material.ui.picture
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+private const val NASA_API_KEY = "b06498a8-768d-4aec-ad83-84d9d309e06d"
+
+class PictureOfTheDayViewModel(
+    private val liveDataForViewToObserve: MutableLiveData<PictureOfTheDayData> = MutableLiveData(),
+    private val retrofitImpl: PODRetrofitImpl = PODRetrofitImpl()
+) :
+    ViewModel() {
+
+    fun getData(): LiveData<PictureOfTheDayData> {
+        sendServerRequest()
+        return liveDataForViewToObserve
+    }
+
+    private fun sendServerRequest() {
+        liveDataForViewToObserve.value = PictureOfTheDayData.Loading(null)
+        val apiKey: String = NASA_API_KEY
+        if (apiKey.isBlank()) {
+            PictureOfTheDayData.Error(Throwable("You need API key"))
+        } else {
+            retrofitImpl.getRetrofitImpl().getPictureOfTheDay(apiKey).enqueue(object :
+                Callback<PODServerResponseData> {
+                override fun onResponse(
+                    call: Call<PODServerResponseData>,
+                    response: Response<PODServerResponseData>
+                ) {
+                    if (response.isSuccessful && response.body() != null) {
+                        liveDataForViewToObserve.value =
+                            PictureOfTheDayData.Success(response.body()!!)
+                    } else {
+                        val message = response.message()
+                        if (message.isNullOrEmpty()) {
+                            liveDataForViewToObserve.value =
+                                PictureOfTheDayData.Error(Throwable("Unidentified error"))
+                        } else {
+                            liveDataForViewToObserve.value =
+                                PictureOfTheDayData.Error(Throwable(message))
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<PODServerResponseData>, t: Throwable) {
+                    liveDataForViewToObserve.value = PictureOfTheDayData.Error(t)
+                }
+            })
+        }
+    }
+}
